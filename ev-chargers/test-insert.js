d1793ed4e1a0b4bbfb574d3ebd9e667aa934694b
Darlynton <darlynton03@gmail.com>
2025-05-17 21:45:19 +0100

    Untrack and ignore local backup JS files

--- ev-chargers/test-insert.js
+++
@@ -1,9 +0,0 @@
-// test-insert.js
-const db = require('./db');
-
-const phone = 'whatsapp:+447400123456';
-const carModel = 'Tesla Model 3';
-
-db.prepare('INSERT OR REPLACE INTO users (phone, car_model) VALUES (?, ?)').run(phone, carModel);
-
-console.log(`Inserted test user: ${phone} - ${carModel}`);
